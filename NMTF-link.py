- modify stop criterion for RMSE - fix code - add normalization step

NMTF-link.py
@@ -1,8 +1,5 @@
 import warnings
 import sys
-import time
-import statistics
-import os
 import numpy as np
 import pandas as pd
 import pylab as plt
@@ -49,7 +46,7 @@ with open(conf_file) as f:
             try:
                 s = line.strip().split("\t")
                 max_iter = int(s[1])
-                print(f"Maximum number of iteration is {bold(str(max_iter))}" )
+                print(f"Maximum number of iteration is {bold(str(max_iter))}")
             except ValueError:
                 print("Number of iterations should be integer")
                 exit(-1)
@@ -81,7 +78,7 @@ if stop_criterion == 'maximum_metric':
     for j in range(5):
         V = []
         difference = []
-        verbose = True if j==0 else False
+        verbose = True if j == 0 else False
 
         network = Network(conf_file, input_dir, verbose)
         initial_error = network.get_error()
@@ -94,18 +91,17 @@ if stop_criterion == 'maximum_metric':
                 metric_vals[i // 10] = V[-1]
                 result = [metric_vals]
             print(f"iteration {str(i + 1)}, {metric} = {V[-1]}")
-            if metric=='rmse':
-                if i>1 and best_iter==0:
+            if metric == 'rmse':
+                if i > 1 and best_iter == 0:
                     difference.append(abs((V[-1] - V[-2]) / V[-2]))
-                    if (difference[-1] < 0.001) and (difference[-1] == min(difference)): #to verify
+                    if (difference[-1] < 0.001) and (difference[-1] == min(difference)):  # to verify
                         best_iter = i
             else:
                 if V[-1] == max(V):
                     best_iter = i
-        print('metric_vals ', metric_vals)
+
         X = np.arange(1, max_iter, 10)
         df = pd.DataFrame([metric_vals], columns=X).melt()
-        print('df ', df)
         sns.lineplot(x="variable", y="value", data=df, ci='sd')
         plt.xlabel('Iteration')
 
@@ -136,13 +132,13 @@ if stop_criterion == 'maximum_metric':
     res_best_iter = statistics.median(best_iter_arr)
     plt.axvline(x=res_best_iter, color='k', label='selected stop iteration', linestyle='dashed')
     plt.legend(loc=4)
-    #plt.ylim(0, 1)
+    # plt.ylim(0, 1)
     plt.savefig('results/' + metric + '_' + network.init_strategy + '_' + stop_criterion + '.png')
     plt.close("all")
 
     network = Network(conf_file, input_dir, mask=0, verbose=False)
     initial_error = network.get_error()
-    print(bold("Final run without masking"))
+    print(f"Final run without masking, stop at iteration: {str(res_best_iter)}")
     # cycle to find new predictions on unmasked matrix
     error_f = []
     epsilon = 0
@@ -153,20 +149,6 @@ if stop_criterion == 'maximum_metric':
             epsilon = abs((error_f[-1] - error_f[-2]) / error_f[-2])
         print(f"iteration {i + 1}, relative error = {epsilon}")
 
-    # reconstruction of the matrix from factor matrices at the final point and output of result
-    rebuilt_association_matrix = np.linalg.multi_dot(
-        [network.get_main().G_left, network.get_main().S, network.get_main().G_right.transpose()])
-    new_relations_matrix = rebuilt_association_matrix - network.get_main().original_matrix
-    n, m = new_relations_matrix.shape
-    outF = open("results/myOutFile.txt", "w")
-    for i in range(n):
-        for j in range(m):
-            if new_relations_matrix[i, j] > threshold:
-                line = network.get_main().left_sorted_term_list[i] + "  " + network.get_main().right_sorted_term_list[
-                    j] + "  " + str(new_relations_matrix[i, j])
-                outF.write(line)
-                outF.write("\n")
-    outF.close()
 
 if stop_criterion == 'relative_error':
     best_epsilon_arr = []
@@ -227,7 +209,7 @@ if stop_criterion == 'relative_error':
 
     res_best_epsilon = statistics.median(best_epsilon_arr)
     plt.axvline(x=res_best_epsilon, color='k', label='selected stop iteration', linestyle='dashed')
-    plt.ylim(0, 1)
+    #plt.ylim(0, 1)
     plt.legend(loc=4)
     plt.savefig('results/' + metric + '_' + network.init_strategy + '_' + stop_criterion + '.png')
     plt.close("all")
@@ -246,20 +228,7 @@ if stop_criterion == 'relative_error':
             epsilon = abs((error_f[-1] - error_f[-2]) / error_f[-2])
         print(f"iteration {i + 1}, relative error = {epsilon}")
 
-    # reconstruction of the matrix from factor matrices at the final point and output of result
-    rebuilt_association_matrix = np.linalg.multi_dot(
-        [network.get_main().G_left, network.get_main().S, network.get_main().G_right.transpose()])
-    new_relations_matrix = rebuilt_association_matrix - network.get_main().original_matrix
-    n, m = new_relations_matrix.shape
-    outF = open("results/myOutFile.txt", "w")
-    for i in range(n):
-        for j in range(m):
-            if new_relations_matrix[i, j] > threshold:
-                line = network.get_main().left_sorted_term_list[i] + "  " + network.get_main().right_sorted_term_list[
-                    j] + "  " + str(new_relations_matrix[i, j])
-                outF.write(line)
-                outF.write("\n")
-    outF.close()
+
 
 elif stop_criterion == 'maximum_iterations':
     network = Network(conf_file, input_dir, mask=0, verbose=True)
@@ -277,18 +246,19 @@ elif stop_criterion == 'maximum_iterations':
             epsilon = abs((error[-1] - error[-2]) / error[-2])
             print(f"iteration {i + 1}, relative error = {epsilon}")
 
-    # reconstruction of the matrix from factor matrices and output of result
-    rebuilt_association_matrix = np.linalg.multi_dot(
-        [network.get_main().G_left, network.get_main().S, network.get_main().G_right.transpose()])
-    new_relations_matrix = rebuilt_association_matrix - network.get_main().original_matrix
-    n, m = new_relations_matrix.shape
-    outF = open("results/myOutFile.txt", "w")
-    for i in range(n):
-        for j in range(m):
-            if new_relations_matrix[i, j] > threshold:
-                line = network.get_main().left_sorted_term_list[i] + "  " + network.get_main().right_sorted_term_list[
-                    j] + "  " + str(new_relations_matrix[i, j])
-                outF.write(line)
-                outF.write("\n")
-    outF.close()
 
+
+# reconstruction of the matrix from factor matrices at the final point and output of result
+rebuilt_association_matrix = np.linalg.multi_dot(
+    [network.get_main().G_left, network.get_main().S, network.get_main().G_right.transpose()])
+new_relations_matrix = rebuilt_association_matrix - network.get_main().original_matrix
+n, m = new_relations_matrix.shape
+outF = open("results/myOutFile.txt", "w")
+for i in range(n):
+    for j in range(m):
+        if new_relations_matrix[i, j] > threshold:
+            line = network.get_main().left_sorted_term_list[i] + "  " + network.get_main().right_sorted_term_list[
+                j] + "  " + str(new_relations_matrix[i, j])
+            outF.write(line)
+            outF.write("\n")
+outF.close()

configurations.py
@@ -1,61 +1,65 @@
 import os
-import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-
 from tqdm import tqdm
 
 stop_criterion = 'maximum_metric'
-init_strategies = ['random', 'skmeans', 'kmeans']
-metrics = ['RMSE', 'R2', 'PEARSON']
-n_iter = range(100,500,100)
-masks = ['fully_random', 'per_row_random']
-#thresholds = np.arange(0, 0.1, 0.001)
+init_strategies = ['random', 'skmeans']  # , 'kmeans']
+metrics = ['RMSE', 'R2']  # , 'PEARSON']
+n_iter = range(100, 300, 100)
+masks = ['fully_random']  # , 'per_row_random']
+normalizations = ['standard_scaler']#, 'min_max', 'max', 'unit_form', 'none']
+# thresholds = np.arange(0, 0.1, 0.001)
 
-#for strategy in init_strategies:
+# for strategy in init_strategies:
 #    for metric in metrics:
 #        for mask in masks:
 threshold = 0.1
-#strategy = 'skmeans'
-#metric = 'RMSE'
-#mask = 'fully_random'
+# strategy = 'skmeans'
+# metric = 'RMSE'
+# mask = 'fully_random'
+for normalization in normalizations:
+    for mask in masks:
+        for strategy in init_strategies:
+            for metric in metrics:
+                rel_error = []
+                iterations = []
+                for n in tqdm(n_iter):
+                    # for threshold in thresholds:
+                    with open('case_study_4/graph_topology_config.tsv', 'w') as f:
+                        f.write("#parameters\n"
+                                "#integration.strategy	intersection\n"
+                                f"#initialization	{strategy}\n"
+                                f"#metric	{metric}\n"
+                                f"#number.of.iterations	{n}\n"
+                                f"#type.of.masking	{mask}\n"
+                                f"#normalization	{normalization}\n"
+                                "#stop.criterion	maximum_metric\n"
+                                f"#score.threshold	{threshold}\n"
+                                "#nodes_left	nodes_right	filename	main\n"
+                                "cell_line	drug	drug_cell.txt   1\n")
 
-for strategy in init_strategies:
-    for metric in metrics:
-        for mask in masks:
-            rel_error = []
-            for n in tqdm(n_iter):
-                #for threshold in thresholds:
-                with open('case_study_4/graph_topology_config.tsv','w') as f:
-                    f.write("#parameters\n"
-                            "#integration.strategy	intersection\n"
-                            f"#initialization	{strategy}\n"
-                            f"#metric	{metric}\n"
-                            f"#number.of.iterations	{n}\n"
-                            f"#type.of.masking	{mask}\n"
-                            "#stop.criterion	maximum_metric\n"
-                            f"#score.threshold	{threshold}\n"
-                            "#nodes_left	nodes_right	filename	main\n"
-                            "cell_line	drug	drug_cell.txt   1\n")
+                    os.system("python NMTF-link.py  case_study_4  graph_topology_config.tsv > results/output.txt")
 
+                    with open("results/output.txt", "r") as f:
+                        x = f.readlines()
+                    l = x[-1].strip().split(',')
+                    l[0] = int(l[0].replace('iteration ', ''))
+                    iterations.append(l[0])
+                    l[1] = float(l[1].replace(' relative error = ', ''))
+                    rel_error.append(l[1])
 
-                os.system("python NMTF-link.py  case_study_4  graph_topology_config.tsv > results/output.txt")
+                df = pd.DataFrame([rel_error], columns=n_iter).melt()
+                df1 = pd.DataFrame([rel_error], columns=iterations).melt()
+                df1 = df1.sort_values('variable')
+                #print(df1)
+                #sns.lineplot(x='variable', y='value', data=df, ci='sd', label=mask + '_' + strategy + '_' + metric)
+                sns.lineplot(x='variable', y='value', data=df1, ci='sd', label=mask + '_' + strategy + '_' + metric)
 
-                with open("results/output.txt", "r") as f:
-                    x = f.readlines()
-                l = x[-1].strip().split(',')
-                l[0] = int(l[0].replace('iteration ', ''))
-                l[1] = float(l[1].replace(' relative error = ', ''))
-                rel_error.append(l[1])
+plt.xlabel('N_Iterations')
+plt.ylabel('Relative Error')
 
-            df = pd.DataFrame([rel_error], columns=n_iter).melt()
-            print(df)
-            sns.lineplot(x = 'variable', y='value', data=df, ci='sd', label=n)
-
-        plt.xlabel('Threshold')
-        plt.ylabel('Relative Error')
-
-        plt.legend(loc=4)
-        plt.savefig('results/configurations/' + metric + '_' + strategy + '_' + stop_criterion + '_' + mask +'.png')
-#plt.close("all")
+plt.legend(loc=4)
+plt.savefig('results/configurations/try.png')  # + metric + '_' + strategy + '_' + stop_criterion + '_' + mask +'.png')
+# plt.close("all")

scripts/AssociationMatrix.py
@@ -1,10 +1,9 @@
 import warnings
-
-warnings.filterwarnings('ignore')
 import networkx as nx
 import numpy as np
 from sklearn.cluster import KMeans
 from spherecluster import SphericalKMeans
+import sklearn.preprocessing as preprocessing
 import sklearn.metrics as metrics
 import copy
 from scripts.utils import bold
@@ -13,6 +12,8 @@ from scipy.stats import pearsonr
 from math import sqrt
 import os
 import sys
+
+warnings.filterwarnings('ignore')
 
 
 @contextmanager
@@ -28,6 +29,7 @@ def suppress_stdout():
 
 class AssociationMatrix():
     def __init__(self, filename, leftds, rightds, left_sorted_terms, right_sorted_terms, main, mask, type_of_masking,
+                 normalization,
                  verbose):
         self.filename = filename
         self.leftds = leftds
@@ -47,6 +49,8 @@ class AssociationMatrix():
         self.validation = mask
         if self.main == 1:
             self.type_of_masking = type_of_masking
+        if normalization != None:
+            self.normalization = normalization
 
         with open(self.filename, "r") as f:
             def split_line(line):
@@ -57,6 +61,7 @@ class AssociationMatrix():
                     return tuple(s + [1])
 
             data_graph = [split_line(element) for element in f.readlines()]
+
         self.edges = [el for el in data_graph
                       if el[0] in set(self.left_sorted_term_list) and el[1] in set(self.right_sorted_term_list)]
 
@@ -71,8 +76,11 @@ class AssociationMatrix():
                                                                                     dtype=np.float)
         self.association_matrix = self.association_matrix.toarray()
 
-
         self.original_matrix = copy.deepcopy(self.association_matrix)  # for all to use in select_rank
+
+        if hasattr(self,normalization):
+            self.normalize()
+
         if self.main == 1 and self.validation == 1:  # so this is the matrix which we try to investigate
             self.mask_matrix()
             # self.compute_tf_idf() #not affecting original matrix
@@ -89,16 +97,16 @@ class AssociationMatrix():
             print(
                 f"Non-zero elements of the association matrix = {bold(str(np.count_nonzero(self.association_matrix)))}")
 
-        if initialize_strategy == "random":
-            if self.G_left is None:
-                self.G_left = np.random.rand(self.association_matrix.shape[0], self.k1)
-                self.G_left_primary = True
-            if self.G_right is None:
-                self.G_right = np.random.rand(self.association_matrix.shape[1], self.k2)
-                self.G_right_primary = True
-        elif initialize_strategy == "oldkmeans":
-            if self.G_left is None:
-                with suppress_stdout():
+        with suppress_stdout():
+            if initialize_strategy == "random":
+                if self.G_left is None:
+                    self.G_left = np.random.rand(self.association_matrix.shape[0], self.k1)
+                    self.G_left_primary = True
+                if self.G_right is None:
+                    self.G_right = np.random.rand(self.association_matrix.shape[1], self.k2)
+                    self.G_right_primary = True
+            elif initialize_strategy == "oldkmeans":
+                if self.G_left is None:
                     km = KMeans(n_clusters=self.k1).fit(self.association_matrix)
                     self.G_left = np.zeros((self.association_matrix.shape[0], self.k1))
                     for row in range(self.association_matrix.shape[0]):
@@ -106,8 +114,7 @@ class AssociationMatrix():
                             self.G_left[row, col] = np.linalg.norm(
                                 self.association_matrix[row] - km.cluster_centers_[col])
                     self.G_left_primary = True
-            if self.G_right is None:
-                with suppress_stdout():
+                if self.G_right is None:
                     km = KMeans(n_clusters=self.k2).fit(self.association_matrix.transpose())
                     self.G_right = np.zeros((self.association_matrix.shape[1], self.k2))
                     for row in range(self.association_matrix.shape[1]):
@@ -115,31 +122,26 @@ class AssociationMatrix():
                             self.G_right[row, col] = np.linalg.norm(
                                 self.association_matrix.transpose()[row] - km.cluster_centers_[col])
                     self.G_right_primary = True
-        elif initialize_strategy == "kmeans":
-            if self.G_left is None:
-                with suppress_stdout():
+            elif initialize_strategy == "kmeans":
+                if self.G_left is None:
                     km = KMeans(n_clusters=self.k1, n_init=10).fit_predict(self.association_matrix.transpose())
                     self.G_left = np.array(
                         [np.mean([self.association_matrix[:, i] for i in range(len(km)) if km[i] == p], axis=0) for p in
                          range(self.k1)]).transpose()
                     self.G_left_primary = True
-            if self.G_right is None:
-                with suppress_stdout():
+                if self.G_right is None:
                     km = KMeans(n_clusters=self.k2, n_init=10).fit_predict(self.association_matrix)
                     self.G_right = np.array(
                         [np.mean([self.association_matrix[i] for i in range(len(km)) if km[i] == p], axis=0) for p in
                          range(self.k2)]).transpose()
                     self.G_right_primary = True
-        elif initialize_strategy == "skmeans":
-            # with suppress_stdout():
-            if self.G_left is None:
-                with suppress_stdout():
+            elif initialize_strategy == "skmeans":
+                if self.G_left is None:
                     skm = SphericalKMeans(n_clusters=self.k1).fit(self.association_matrix.transpose())
                     # Factor matrices are initialized with the center coordinates
                     self.G_left = skm.cluster_centers_.transpose()
                     self.G_left_primary = True
-            if self.G_right is None:
-                with suppress_stdout():
+                if self.G_right is None:
                     skm = SphericalKMeans(n_clusters=self.k2).fit(self.association_matrix)
                     # Factor matrices are initialized with the center coordinates
                     self.G_right = skm.cluster_centers_.transpose()
@@ -162,6 +164,7 @@ class AssociationMatrix():
             print(self.leftds, self.rightds, self.association_matrix.shape)
             print(f"Shape Factor Matrix left {str(self.G_left.shape)}")
             print(f"Shape Factor Matrix right {str(self.G_right.shape)}\n")
+
         self.S = np.linalg.multi_dot([self.G_left.transpose(), self.association_matrix, self.G_right])
 
     # NON USATA, IMPLEMENTATA PER TRASFORMAZIONE MA NO RISULTATI MIGLIORI
@@ -238,7 +241,6 @@ class AssociationMatrix():
                 elif metric == 'rmse':
                     return sqrt(metrics.mean_squared_error(R12_2, R12_found_2))
 
-
     def get_error(self):
         self.rebuilt_association_matrix = np.linalg.multi_dot([self.G_left, self.S, self.G_right.transpose()])
         return np.linalg.norm(self.association_matrix - self.rebuilt_association_matrix, ord='fro') ** 2
@@ -296,3 +298,13 @@ class AssociationMatrix():
         if self.G_left_primary:
             self.G_left = self.update_G_left()
         self.S = self.update_S()
+
+    def normalize(self):
+        if self.normalization == 'min_max':
+            self.association_matrix = preprocessing.minmax_scale(self.association_matrix)
+        elif self.normalization == 'standard_scaler':
+            self.association_matrix = preprocessing.StandardScaler().fit_transform(self.association_matrix)
+        elif self.normalization == 'max':
+            self.association_matrix = preprocessing.maxabs_scale(self.association_matrix)
+        elif self.normalization == 'unit_form':
+            self.association_matrix = preprocessing.normalize(self.association_matrix)

scripts/Network.py
@@ -1,20 +1,22 @@
 import warnings
-warnings.filterwarnings('ignore')
 import sys
-sys.path.insert(0, 'scripts/')
-from AssociationMatrix import AssociationMatrix
-from scripts.utils import bold
 import numpy as np
 import os
+from scripts.AssociationMatrix import AssociationMatrix
+from scripts.utils import bold
+
+warnings.filterwarnings('ignore')
+sys.path.insert(0, 'scripts/')
+
 
 class Network():
-
-    def __init__(self, graph_topology_file,dirfilename,verbose, mask = 1):
+    def __init__(self, graph_topology_file, dirfilename, verbose, mask=1):
         self.init_strategy = "random"
         self.integration_strategy = lambda x, y: x.intersection(y)
         self.association_matrices = []
         self.datasets = {}
-        self.type_of_masking=1
+        self.type_of_masking = 1
+        self.normalization = None
 
         with open(graph_topology_file) as f:
             for line in f:
@@ -27,6 +29,7 @@ class Network():
                     else:
                         print(f"Option '{s[1]}' not supported")
                         exit(-1)
+
                 if line.strip().startswith("#initialization"):
                     s = line.strip().split("\t")
                     if s[1] == "random":
@@ -38,8 +41,9 @@ class Network():
                     else:
                         print(f"Option '{s[1]}' not supported")
                         exit(-1)
-                    if verbose==True:
+                    if verbose == True:
                         print(f"Initialization strategy is {bold(self.init_strategy)}\n")
+
                 if line.strip().startswith("#type.of.masking"):
                     s = line.strip().split("\t")
                     if s[1] == "fully_random":
@@ -50,23 +54,40 @@ class Network():
                         print(f"Option '{s[1]}' not supported")
                         exit(-1)
 
-        #For each category of nodes, compute the intersection or union between the different matrices
-        with open(graph_topology_file) as f:
-            for line in f:
+                if line.strip().startswith("#normalization"):
+                    s = line.strip().split("\t")
+                    if s[1].lower() == "min_max":
+                        self.normalization = "min_max"
+                    elif s[1].lower() == "standard_scaler":
+                        self.normalization = "standard_scaler"
+                    elif s[1].lower() == "max":
+                        self.normalization = "max"
+                    elif s[1].lower() == "unit_form":
+                        self.normalization = "unit_form"
+                    elif s[1].lower() == "none":
+                        self.normalization = None
+                    else:
+                        print(f"Option '{s[1]}' not supported")
+                        exit(-1)
+
+                # For each category of nodes, compute the intersection or union between the different matrices
                 if not line.strip().startswith("#") and not line.strip().startswith("+"):
                     s = line.strip().split()
                     filename = s[2]
-                    filename = os.path.join(dirfilename,filename)
+                    filename = os.path.join(dirfilename, filename)
                     ds1_name = s[0].upper()
                     ds2_name = s[1].upper()
+                    main = int(s[3])
 
                     ds1_entities = set()
                     ds2_entities = set()
+
                     with open(filename) as af:
                         for edge in af:
                             s_edge = edge.strip().split("\t")
                             ds1_entities.add(s_edge[0])
                             ds2_entities.add(s_edge[1])
+
                     if ds1_name in self.datasets:
                         self.datasets[ds1_name] = self.integration_strategy(self.datasets[ds1_name], ds1_entities)
                     else:
@@ -77,23 +98,11 @@ class Network():
                     else:
                         self.datasets[ds2_name] = ds2_entities
 
-        #sort the nodes, such that each matrix receives the same ordered list of nodes
-        for k in self.datasets.keys():
-            self.datasets[k] = list(sorted(list(self.datasets[k])))
+                    # TODO: OTTIMIZZARE, AD OGNI CICLO FA IL FOR
+                    # sort the nodes, such that each matrix receives the same ordered list of nodes
+                    for k in self.datasets.keys():
+                        self.datasets[k] = list(sorted(list(self.datasets[k])))
 
-        if verbose==True:
-            print(f'All specified nodes\' categories: {bold(str(list(self.datasets.keys())))}\n')
-
-        with open(graph_topology_file) as f:
-            for line in f:
-                if not line.strip().startswith("#")  and not line.strip().startswith("+"):
-                    s = line.strip().split()
-                    filename = s[2]
-                    filename = os.path.join(dirfilename,filename)
-                    ds1_name = s[0].upper()
-                    ds2_name = s[1].upper()
-                    main=int(s[3])
-                    
                     self.association_matrices.append(
                         AssociationMatrix(filename,
                                           ds1_name,
@@ -103,7 +112,11 @@ class Network():
                                           main,
                                           mask,
                                           self.type_of_masking,
+                                          self.normalization,
                                           verbose))
+
+        if verbose == True:
+            print(f'All specified nodes\' categories: {bold(str(list(self.datasets.keys())))}\n')
 
 
         for m1 in self.association_matrices:
@@ -111,52 +124,53 @@ class Network():
                 if m1 != m2:
                     if m1.leftds == m2.leftds:
                         m1.dep_own_left_other_left.append(m2)
-                        #m2.dep_own_left_other_left.append(m1)
+                        # m2.dep_own_left_other_left.append(m1)
                     elif m1.rightds == m2.rightds:
                         m1.dep_own_right_other_right.append(m2)
-                       #m2.dep_own_right_other_right.append(m1)
+                    # m2.dep_own_right_other_right.append(m1)
                     elif m1.rightds == m2.leftds:
                         m1.dep_own_right_other_left.append(m2)
-                        #m2.dep_own_left_other_right.append(m1)
+                        # m2.dep_own_left_other_right.append(m1)
                     elif m1.leftds == m2.rightds:
                         m1.dep_own_left_other_right.append(m2)
-                        #m2.dep_own_right_other_left.append(m1)
-
+                        # m2.dep_own_right_other_left.append(m1)
 
         for k in self.datasets.keys():
-            if  not k.strip().startswith("+"):
+            if not k.strip().startswith("+"):
                 rank = self.select_rank(k)
-          
 
             for am in self.association_matrices:
-                if am.leftds==k:
+                if am.leftds == k:
                     am.k1 = int(rank)
-                elif am.rightds==k:
+                elif am.rightds == k:
                     am.k2 = int(rank)
 
-        
         for am in self.association_matrices:
-            am.initialize(self.init_strategy,verbose)
-            #am.compute_tf_idf()
+            am.initialize(self.init_strategy, verbose)
+            # am.compute_tf_idf()
 
         for am in self.association_matrices:
             am.create_update_rules()
 
-#method to calculate rank for each datatype. In case of k-means and spherical k-means initialization represents number of clusters.
+    # method to calculate rank for each datatype. In case of k-means and spherical k-means initialization represents number of clusters.
     def select_rank(self, ds_name):
 
         if self.init_strategy == "kmeans" or self.init_strategy == "skmeans":
-            el_num= len(self.datasets[ds_name])
-            if el_num>200:
-                el_num=int(el_num/5)
-            for am in self.association_matrices: #rank should be less then the number of unique elements in rows and in columns of any matrix where datatype is present 
-                if am.leftds==ds_name:
-                    el_num=min([el_num, np.count_nonzero(np.sum(np.unique(am.association_matrix, axis= 1), 0)), np.count_nonzero(np.sum(np.unique(am.association_matrix, axis= 0), 1)), len(self.datasets[am.rightds])])
-                elif am.rightds==ds_name:
-                    el_num=min([el_num, np.count_nonzero(np.sum(np.unique(am.association_matrix, axis= 1), 0)), np.count_nonzero(np.sum(np.unique(am.association_matrix, axis= 0), 1)), len(self.datasets[am.leftds])])
-            rank=el_num
+            el_num = len(self.datasets[ds_name])
+            if el_num > 200:
+                el_num = int(el_num / 5)
+            for am in self.association_matrices:  # rank should be less then the number of unique elements in rows and in columns of any matrix where datatype is present
+                if am.leftds == ds_name:
+                    el_num = min([el_num, np.count_nonzero(np.sum(np.unique(am.association_matrix, axis=1), 0)),
+                                  np.count_nonzero(np.sum(np.unique(am.association_matrix, axis=0), 1)),
+                                  len(self.datasets[am.rightds])])
+                elif am.rightds == ds_name:
+                    el_num = min([el_num, np.count_nonzero(np.sum(np.unique(am.association_matrix, axis=1), 0)),
+                                  np.count_nonzero(np.sum(np.unique(am.association_matrix, axis=0), 1)),
+                                  len(self.datasets[am.leftds])])
+            rank = el_num
         elif self.init_strategy == "random":
-            rank=100
+            rank = 100
         return rank
 
     def get_error(self):
@@ -168,11 +182,10 @@ class Network():
 
     def validate(self, metric='aps'):
         for am in self.association_matrices:
-            if am.main==1 and am.validation==1:
+            if am.main == 1 and am.validation == 1:
                 return am.validate(metric)
 
     def get_main(self):
         for am in self.association_matrices:
-            if am.main==1:
+            if am.main == 1:
                 return am
-