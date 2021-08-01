24b6c90a28362e168d78b2b8f5e23cb00712a24e
pp86 <pietro.pinoli@gmail.com>
2021-08-01 17:33:38 +0200

    - some code refactoring and cleaning

--- NMTF-link.py
+++ NMTF-link.py
@@ -4,13 +4,11 @@ warnings.filterwarnings('ignore')
 import sys
 from scripts import Network
 import numpy as np
-import pandas as pd
 import matplotlib
 from utils import EvaluationMetric, StopCriterion
 
 matplotlib.use('agg')
 import pylab as plt
-import seaborn as sns
 import time
 import statistics
 import os
@@ -27,6 +25,39 @@ threshold=default_threshold
 metric = 'aps'
 max_iter =200
 stop_criterion= 'calculate'
+
+def plot_iteration(max_it, met_val):
+    X = np.arange(1, max_it, 10)
+    plt.plot(X, met_val)
+
+def complete_plot(m, y_lim):
+    plt.xlabel('Iteration')
+    if m == EvaluationMetric.APS:
+        plt.ylabel('Average Precision Score (APS)')
+    elif m == EvaluationMetric.AUROC:
+        plt.ylabel('Area Under ROC Curve')
+    plt.ylim(y_lim)
+
+def predict(num_iterations, th):
+    network = Network(dirname_1, dirname_2, mask=0, verbose=False)
+    initial_error = network.get_error()
+
+    for i in range(num_iterations):
+        network.update()
+        print(f"iteration {i}, error = {network.get_error()}")
+
+    rebuilt_association_matrix = np.linalg.multi_dot(
+        [network.get_main().G_left, network.get_main().S, network.get_main().G_right.transpose()])
+    new_relations_matrix = rebuilt_association_matrix - network.get_main().original_matrix
+    n, m = new_relations_matrix.shape
+    with open("results/myOutFile.txt", "w") as outF:
+        for i in range(n):
+            for j in range(m):
+                if new_relations_matrix[i, j] > th:
+                    line = network.get_main().left_sorted_term_list[i] + "  " + network.get_main().right_sorted_term_list[
+                        j] + "  " + str(new_relations_matrix[i, j])
+                    outF.write(line)
+                    outF.write("\n")
 
 
 with open(dirname_1) as f:
@@ -85,54 +116,20 @@ if stop_criterion == StopCriterion.MAXIMUM_METRIC:
             print("iteration {}, ".format(i + 1) + metric + " = {}".format(V[-1]))
             if V[-1] == max(V):
                 best_iter = i
-        X = np.arange(1, max_iter, 10)
-        df = pd.DataFrame([metric_vals], columns=X).melt()
-        sns.lineplot(x="variable", y="value", data=df, ci='sd')
-        plt.xlabel('Iteration')
-        if metric == EvaluationMetric.APS:
-            plt.ylabel('Average Precision Score (APS)')
-        elif metric == EvaluationMetric.AUROC:
-            plt.ylabel('Area Under ROC Curve')
-        if j == 4:
-            plt.savefig(f'results/{metric.value}_{network.init_strategy}_{stop_criterion.value}.png')
+        plot_iteration(max_iter, metric_vals)
         best_iter_arr.append(best_iter)
         best_iter = 0
         time.sleep(2)  # used since otherwise random initialization gives the same result multiple times
 
+    complete_plot(metric, y_lim=(0,1))
+
     res_best_iter = statistics.median(best_iter_arr)
     plt.axvline(x=res_best_iter, color='k', label='selected stop iteration', linestyle='dashed')
     plt.legend(loc=4)
-    plt.ylim(0, 1)
     plt.savefig(f'results/{metric.value}_{network.init_strategy}_{stop_criterion.value}.png')
     plt.close("all")
 
-    network = Network(dirname_1, dirname_2, mask=0, verbose=False)
-    initial_error = network.get_error()
-    print('\033[1m' + "Final run without masking" + '\033[0m')
-    # cycle to find new predictions on unmasked matrix
-    error_f = []
-    epsilon = 0
-    for i in range(res_best_iter):
-        network.update()
-        error_f.append(network.get_error())
-        if i > 1:
-            epsilon = abs((error_f[-1] - error_f[-2]) / error_f[-2])
-        print("iteration {}, relative error = {}".format(i + 1, epsilon))
-
-    # reconstruction of the matrix from factor matrices at the final point and output of result
-    rebuilt_association_matrix = np.linalg.multi_dot(
-        [network.get_main().G_left, network.get_main().S, network.get_main().G_right.transpose()])
-    new_relations_matrix = rebuilt_association_matrix - network.get_main().original_matrix
-    n, m = new_relations_matrix.shape
-    outF = open("results/myOutFile.txt", "w")
-    for i in range(n):
-        for j in range(m):
-            if new_relations_matrix[i, j] > threshold:
-                line = network.get_main().left_sorted_term_list[i] + "  " + network.get_main().right_sorted_term_list[
-                    j] + "  " + str(new_relations_matrix[i, j])
-                outF.write(line)
-                outF.write("\n")
-    outF.close()
+    predict(res_best_iter, threshold)
 
 elif stop_criterion == StopCriterion.RELATIVE_ERROR:
     best_epsilon_arr = []
@@ -163,82 +160,23 @@ elif stop_criterion == StopCriterion.RELATIVE_ERROR:
                     eps_iter.append(i)
             print("iteration {}, relative error = {}".format(i + 1, epsilon))
 
-        X = np.arange(1, max_iter, 10)
-        df = pd.DataFrame([metric_vals], columns=X).melt()
-        sns.lineplot(x="variable", y="value", data=df, ci='sd')
-        plt.xlabel('Iteration')
-        if metric == EvaluationMetric.APS:
-            plt.ylabel('Average Precision Score (APS)')
-        elif metric == EvaluationMetric.AUROC:
-            plt.ylabel('Area Under ROC Curve')
-        if j == 4:
-            plt.savefig(f'results/{metric.value}_{network.init_strategy}_{stop_criterion.value}.png')
-        best_epsilon_arr.append(eps_iter[0])
+        plot_iteration(max_iter, metric_vals)
         time.sleep(2)  # used since otherwise random initialization gives the same result multiple times
+        best_epsilon_arr.append(eps_iter[0])
+
+    complete_plot(metric, y_lim=(0,1))
 
     res_best_epsilon = statistics.median(best_epsilon_arr)
     plt.axvline(x=res_best_epsilon, color='k', label='selected stop iteration', linestyle='dashed')
-    plt.ylim(0, 1)
     plt.legend(loc=4)
     plt.savefig('results/' + metric.value + '_' + network.init_strategy + '_' + stop_criterion.value + '.png')
     plt.close("all")
 
-    network = Network(dirname_1, dirname_2, mask=0, verbose=False)
-    initial_error = network.get_error()
-    prev_error = initial_error
     print('\033[1m' + "Final run without masking, stop at iteration: " + str(res_best_epsilon) + '\033[0m')
-    # cycle to find new predictions on unmasked matrix
-    error_f = []
-    epsilon = 0
-    for i in range(res_best_epsilon):
-        network.update()
-        error_f.append(network.get_error())
-        if i > 1:
-            epsilon = abs((error_f[-1] - error_f[-2]) / error_f[-2])
-        print("iteration {}, relative error = {}".format(i + 1, epsilon))
-
-    # reconstruction of the matrix from factor matrices at the final point and output of result
-    rebuilt_association_matrix = np.linalg.multi_dot(
-        [network.get_main().G_left, network.get_main().S, network.get_main().G_right.transpose()])
-    new_relations_matrix = rebuilt_association_matrix - network.get_main().original_matrix
-    n, m = new_relations_matrix.shape
-    outF = open("results/myOutFile.txt", "w")
-    for i in range(n):
-        for j in range(m):
-            if new_relations_matrix[i, j] > threshold:
-                line = network.get_main().left_sorted_term_list[i] + "  " + network.get_main().right_sorted_term_list[
-                    j] + "  " + str(new_relations_matrix[i, j])
-                outF.write(line)
-                outF.write("\n")
-    outF.close()
+    predict(res_best_epsilon, threshold)
 
 elif stop_criterion == StopCriterion.MAXIMUM_ITERATIONS:
     network = Network(dirname_1, dirname_2, mask=0, verbose=True)
     initial_error = network.get_error()
     print('\033[1m' + "Unique run of the algorithm without masking" + '\033[0m')
-    print("initial error: {}".format(initial_error))
-    error = []
-    for i in range(max_iter):
-        network.update()
-        error.append(network.get_error())
-        if i % 10 == 0:
-            metric_vals[i // 10] = network.validate(metric)
-            result = [metric_vals]
-        if i > 1:
-            epsilon = abs((error[-1] - error[-2]) / error[-2])
-        print("iteration {}, relative error = {}".format(i + 1, epsilon))
-
-    # reconstruction of the matrix from factor matrices and output of result
-    rebuilt_association_matrix = np.linalg.multi_dot(
-        [network.get_main().G_left, network.get_main().S, network.get_main().G_right.transpose()])
-    new_relations_matrix = rebuilt_association_matrix - network.get_main().original_matrix
-    n, m = new_relations_matrix.shape
-    outF = open("results/myOutFile.txt", "w")
-    for i in range(n):
-        for j in range(m):
-            if new_relations_matrix[i, j] > threshold:
-                line = network.get_main().left_sorted_term_list[i] + "  " + network.get_main().right_sorted_term_list[
-                    j] + "  " + str(new_relations_matrix[i, j])
-                outF.write(line)
-                outF.write("\n")
-    outF.close()
+    predict(max_iter, threshold)
